78317748ba10b6ce46754fbccf9ef13aa16ca18e
Mark-DSouza <markdsouza434@gmail.com>
2023-05-25 23:11:47 +0530

    Add /random endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,37 @@ use diesel::prelude::*;
 use rocket::serde::json::Json;
 use rocket::{Build, Rocket};
 
+use rand::Rng;
+
 use self::models::*;
 use self::schema::quotes::dsl::*;
 
 mod database;
 mod models;
 mod schema;
+
+#[get("/random")]
+fn get_random_quote() -> Json<Quote> {
+    let connection = &mut database::establish_connection();
+
+    let all_quotes = quotes
+        .load::<Quote>(connection)
+        .map(Json)
+        .expect("Error loading birds");
+
+    let range = 0..all_quotes.len() as i32;
+    let random_number_in_range = rand::thread_rng().gen_range(range);
+    let random_id = random_number_in_range + 1;
+
+    let quote: &Quote = all_quotes.iter().find(|&x| x.id == random_id).unwrap();
+
+    Json(Quote {
+        id: quote.id.clone(),
+        author: quote.author.clone(),
+        content: quote.content.clone(),
+        category: quote.category.clone(),
+    })
+}
 
 #[get("/")]
 fn index() -> Json<Vec<Quote>> {
@@ -23,5 +48,5 @@ fn index() -> Json<Vec<Quote>> {
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, get_random_quote])
 }
